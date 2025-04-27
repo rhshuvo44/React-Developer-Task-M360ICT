58f9bb45452997c02e822f35e7390ca1f3010881
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2025-04-27 23:20:11 +0600

    fact:products show on table with pagination

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,5 +1,87 @@
+import { Button, Table } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Product, useGetProductsQuery } from "../redux/api/api";
+import Loading from "./ui/Loading";
+
 const ProductTable = () => {
-  return <div></div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const { data, isError, isLoading } = useGetProductsQuery({
+    limit: pageSize,
+    skip: (currentPage - 1) * pageSize,
+  });
+  const navigate = useNavigate();
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+    },
+    {
+      title: "Brand",
+      dataIndex: "brand",
+      key: "brand",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Stock",
+      dataIndex: "stock",
+      key: "stock",
+    },
+    {
+      title: "Rating",
+      dataIndex: "rating",
+      key: "rating",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_: number, record: Product) => (
+        <Button type="link" onClick={() => navigate(`/product/${record.id}`)}>
+          View Details
+        </Button>
+      ),
+    },
+  ];
+
+  if (isLoading) return <Loading />;
+  if (isError) return <div>Error: {isError}</div>;
+
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-center mb-2">Product List</h1>
+      <div className="responsive-table-container">
+        <Table
+          className="table-auto"
+          bordered
+          columns={columns}
+          dataSource={data?.products}
+          rowKey="id"
+          pagination={{
+            current: currentPage,
+            pageSize: pageSize,
+            total: data?.total,
+            onChange: (page, pageSize) => {
+              setCurrentPage(page);
+              setPageSize(pageSize);
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProductTable;
--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,14 +1,37 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    thumbnail: string;
+    category: string,
+    brand: string,
+    sku: string
+    stock: number;
+    rating: number
+    tags: string[]
+    images: string[];
+    reviews: {
+        rating: number,
+        comment: string,
+        reviewerName: string,
+    }[];
+
+}
 
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/' }),
     endpoints: (builder) => ({
+        getProducts: builder.query<{ products: Product[]; total: number }, { limit: number; skip: number }>({
+            query: ({ limit, skip }) => ({ url: `products?limit=${limit}&skip=${skip}`, method: 'GET' }),
+        }),
 
     }),
 })
 
-export const { } = baseApi
+export const { useGetProductsQuery } = baseApi
